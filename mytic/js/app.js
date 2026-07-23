f64c3e00077d75984df35463c981d8af6adc9bc8
hazelhudaya96 <hudayahasel@gmail.com>
2026-07-23 17:19:47 +0700

    Fix Uncaught SyntaxError in mobile navigation template string

--- mytic/js/app.js
+++ mytic/js/app.js
@@ -388,6 +388,7 @@ function setupNavigation() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="${m.icon}"/></svg>
             <span>${labelWithBadge}</span>
           </a>`;
+      }
     }).join('')}</div>
       </div>`;
     document.body.appendChild(popup);
